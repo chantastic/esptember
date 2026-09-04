be887f7f88e9ee3035d633419117271850f98f39
Michael Chan <mijoch@gmail.com>
2026-09-04 10:01:33 -0700

    Feature the ESPtember introduction video at the top of pages

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,1 +1,6 @@
 export const GITHUB_REPO = "https://github.com/chantastic/esptember";
+
+export const INTRO_VIDEO = {
+  url: "https://www.youtube.com/shorts/DeUNA-bTqI4",
+  title: "Happy ESPtember to all who celebrate",
+};
